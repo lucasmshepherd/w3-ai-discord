61c647a6dc47b9b451748df10003a7bc49f4884c
Jun <love.code.ninja@gmail.com>
2023-07-25 15:40:47 -0500

    added button response

--- commands/ask.js
+++ commands/ask.js
@@ -1,10 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ask')
 		.setDescription('Ask anything!'),
 	async execute(interaction) {
-		await interaction.reply('Puddle Jumper can help you!');
+		const openApp = new ButtonBuilder()
+			.setLabel('Open App')
+			.setURL('https://rewardz.network')
+			.setStyle(ButtonStyle.Link);
+
+		const row = new ActionRowBuilder()
+			.addComponents(openApp);
+
+		await interaction.reply({
+			content: 'Puddle Jumper can help you!',
+			components: [row],
+		});
 	},
 };
